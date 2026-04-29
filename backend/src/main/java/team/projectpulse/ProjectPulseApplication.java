ec798ebc100a45328580bc2f1a55c98eee9e889d
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-04-29 10:30:08 -0500

    fixed small typo

--- backend/src/main/java/team/projectpulse/ProjectPulseApplication.java
+++ backend/src/main/java/team/projectpulse/ProjectPulseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "team.projectpulse")
-public class git ProjectPulseApplication {
+public class ProjectPulseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectPulseApplication.class, args);
