cc209d64dd407c973232a821c4cf5bb7a59cd234
Kaylee Webb <kayleewebb@MacBook-Pro-2.local>
2026-04-28 21:08:56 -0500

    small changes

--- backend/src/main/java/team/projectpulse/ProjectPulseApplication.java
+++ backend/src/main/java/team/projectpulse/ProjectPulseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "team.projectpulse")
-public class ProjectPulseApplication {
+public class git ProjectPulseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectPulseApplication.class, args);
